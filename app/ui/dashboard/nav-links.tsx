1a529b83c789068f21ed894a9f16747f9903216e
yeshwanth9py <18131a03l2@gvpce.ac.in>
2024-09-12 13:06:06 +0530

    m-5

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
